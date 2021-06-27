8946db6340831d7a0e38779e1733fc211244ef94
Years <Years_CN@163.com>
2021-06-27 21:20:06 +0800

    songList

---
+++ src/pages/listDetail/components/songList.jsx
@@ -0,0 +1,44 @@
+import { View } from '@tarojs/components'
+import { useEffect } from 'react'
+import './songList.scss'
+import { AtIcon } from 'taro-ui'
+
+export default function SongList(props) {
+
+  let nameList = props.list.map((item) => {
+      let i = item.ar.reduce((pre, cur) => {
+          return {name: pre.name + '/' + cur.name};
+      },[{name:''}]);
+      return i = i.name.slice(i.name.indexOf('/') + 1) + ' - ' + item.name;  
+    }
+  )
+  console.log(props.list);
+  console.log(nameList);
+
+  return (
+    <View className="song_list">
+      <View className="play_all">
+        <View className='play_icon'>
+          <AtIcon value='play' size='20' color='gary'></AtIcon>
+        </View>
+        <View className='title'>播放全部</View>
+      </View>
+      {
+        // props.list.length === 0 ? '' :
+        props.list.map((item, index) => 
+          <View key={index} className="song">
+            <View className="song_index">{index+1}</View>
+            <View className="song_info">
+              <View className="song_name">{item.name}</View>
+              <View className="ar">{nameList[index]}</View>
+              
+            </View>
+            <View className="icon">
+              <AtIcon  value='play' size='20' color='gary'></AtIcon>
+            </View>
+          </View>
+        )
+      }
+    </View>
+  )
+}
--- src/pages/listDetail/index.jsx
+++ src/pages/listDetail/index.jsx
@@ -3,9 +3,11 @@ import Taro from '@tarojs/taro'
 import { useState, useEffect } from 'react'
 import http from '../../servers/http'
 import './index.scss'
+import SongList from './components/songList'
 
 export default function ListDetail(props) {
   const [info, setInfo] = useState([]);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
     Taro.setNavigationBarTitle({
@@ -15,10 +17,13 @@ export default function ListDetail(props) {
     http.get(`/playlist/detail?id=${Taro.getCurrentInstance().router.params.id}`).then(res => {
       console.log(res);
       setInfo(res);
-    }) 
+      setList(res.playlist.tracks);
+      return res;
+    }).then(res => {
 
-
+    })
   }, [])
+
 
   return (
     <View className="list_detail">
@@ -27,22 +32,27 @@ export default function ListDetail(props) {
       </header>
       {
         info.length === 0 ? "" : 
-        <View className="info"> 
-          <Image src={info.playlist.coverImgUrl} className="info_img"/>
-          <View className="info_detail">
-            <View className="info_name">
-              {info.playlist.name}
-            </View>
-            <View className="avatar">
-              <Image src={info.playlist.creator.avatarUrl} className="avatar_img"/>
-              <View className="avatar_name">{info.playlist.creator.nickname}</View>
-            </View>
-            <View className="info_description">
-              {info.playlist.description}
+        <>
+          <View className="info"> 
+            <Image src={info.playlist.coverImgUrl} className="info_img"/>
+            <View className="info_detail">
+              <View className="info_name">
+                {info.playlist.name}
+              </View>
+              <View className="avatar">
+                <Image src={info.playlist.creator.avatarUrl} className="avatar_img"/>
+                <View className="avatar_name">{info.playlist.creator.nickname}</View>
+              </View>
+              <View className="info_description">
+                {info.playlist.description}
+              </View>
             </View>
           </View>
-        </View>
+          <View style="height:50px"></View>
+          <SongList list={info.playlist.tracks}/>
+        </>
       }
+      
       
     </View>
   )
